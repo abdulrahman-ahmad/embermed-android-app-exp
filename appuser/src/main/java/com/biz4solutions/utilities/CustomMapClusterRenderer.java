d3e961ba2f6f9490c115e7a3d31033b8625fb9cb
karan <karan.bajaj@biz4solutions.com>
2018-09-11 16:11:05 +0530

    -Issue fixing

---
+++ appuser/src/main/java/com/biz4solutions/utilities/CustomMapClusterRenderer.java
@@ -0,0 +1,46 @@
+package com.biz4solutions.utilities;
+
+import android.content.Context;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.clustering.Cluster;
+import com.google.maps.android.clustering.ClusterManager;
+import com.google.maps.android.clustering.view.DefaultClusterRenderer;
+
+/**
+ * custom map cluster renderer to set cluster size
+ */
+
+public class CustomMapClusterRenderer<MapClusterItem extends MarkerClusterItem> extends DefaultClusterRenderer<MapClusterItem> {
+    public CustomMapClusterRenderer(Context context, GoogleMap map, ClusterManager<MapClusterItem> clusterManager) {
+        super(context, map, clusterManager);
+    }
+
+    @Override
+    protected boolean shouldRenderAsCluster(Cluster<MapClusterItem> cluster) {
+        //start clustering if at least 2 items overlap
+        return cluster.getSize() > Constants.MINIMUM_CLUSTER_SIZE;
+    }
+
+    @Override
+    protected void onClusterItemRendered(MapClusterItem clusterItem, Marker marker) {
+        super.onClusterItemRendered(clusterItem, marker);
+        clusterItem.setMarker(marker);
+    }
+
+    @Override
+    protected void onBeforeClusterItemRendered(MapClusterItem item,
+                                               MarkerOptions markerOptions) {
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(
+                BitmapDescriptorFactory.HUE_GREEN));
+//        markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_marker_victim));
+    }
+
+    @Override
+    public void onRemove() {
+        super.onRemove();
+    }
+}
