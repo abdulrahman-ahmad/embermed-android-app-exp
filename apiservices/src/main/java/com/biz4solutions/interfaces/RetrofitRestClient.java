221c09320e2f951dea88f5e86d4a6a5da561a70c
ketan <ketan@biz4solutions.com>
2018-10-04 12:15:40 +0530

    update lib version

--- apiservices/src/main/java/com/biz4solutions/interfaces/RetrofitRestClient.java
+++ apiservices/src/main/java/com/biz4solutions/interfaces/RetrofitRestClient.java
@@ -33,114 +33,116 @@ import retrofit2.http.Query;
 
 public interface RetrofitRestClient {
 
-    @POST("v1/users/login")
+    @POST("/api/v1/users/login")
     Call<LoginResponse> doLogin(@Body LoginRequest loginRequest);
 
-    @POST("v1/provider/login")
+    @POST("/api/v1/provider/login")
     Call<LoginResponse> doProviderLogin(@Body LoginRequest loginRequest);
 
-    @POST("v1/users/socialAppLogin")
+    @POST("/api/v1/users/socialAppLogin")
     Call<LoginResponse> socialAppLogin(@Body SocialMediaUserData socialSignInDTO);
 
-    @POST("v1/users/signup")
+    @POST("/api/v1/users/signup")
     Call<LoginResponse> signUp(@Body SignUpRequest signUpRequest);
 
-    @POST("v1/otp/requestOtp")
+    @POST("/api/v1/otp/requestOtp")
     Call<EmptyResponse> requestOtp(@Body HashMap<String, String> body);
 
-    @POST("v1/otp/resendOtp")
+    @POST("/api/v1/otp/resendOtp")
     Call<EmptyResponse> resendOtp(@Body HashMap<String, String> body);
 
-    @POST("v1/otp/verify")
+    @POST("/api/v1/otp/verify")
     Call<EmptyResponse> verifyOtp(@Body HashMap<String, Object> body);
 
-    @POST("v1/user/resetPassword")
+    @POST("/api/v1/user/resetPassword")
     Call<EmptyResponse> resetPassword(@Body HashMap<String, Object> body);
 
-    @PUT("v1/users/updateFcmToken")
+    @PUT("/api/v1/users/updateFcmToken")
     Call<EmptyResponse> setFcmToken(@Body HashMap<String, Object> body);
 
-    @POST("v1/users/sendRequest")
+    @POST("/api/v1/users/sendRequest")
     Call<CreateEmsResponse> createRequest(@Body CreateEmsRequest body);
 
-    @DELETE("v1/users/cancelRequest")
+    @DELETE("/api/v1/users/cancelRequest")
     Call<EmptyResponse> cancelRequest(@Query("requestId") String query);
 
-    @GET("v1/provider/request/list")
+    @GET("/api/v1/provider/request/list")
     Call<EmsRequestResponse> getRequestList(@Query("page") int page, @Query("size") int size);
 
-    @PUT("v1/provider/request/accept")
+    @PUT("/api/v1/provider/request/accept")
     Call<EmptyResponse> acceptRequest(@Query("requestId") String requestId);
 
-    @POST("v1/provider/incidentReport/submit")
+    @POST("/api/v1/provider/incidentReport/submit")
     Call<EmptyResponse> submitIncidentReport(@Body IncidentReport body);
 
-    @DELETE("v1/provider/request/reject")
+    @DELETE("/api/v1/provider/request/reject")
     Call<EmptyResponse> rejectRequest(@Query("requestId") String requestId);
 
-    @PUT("v1/provider/request/complete")
+    @PUT("/api/v1/provider/request/complete")
     Call<EmptyResponse> completeRequest(@Body HashMap<String, Object> body);
 
-    @GET("v1/provider/requestDetail")
+    @GET("/api/v1/provider/requestDetail")
     Call<EmsRequestDetailsResponse> getRequestDetails(@Query("requestId") String requestId);
 
+    // google api
     @GET("api/directions/json")
     Call<GoogleDirectionResponse> getDirections(@Query("units") String units, @Query("origin") String origin, @Query("destination") String destination, @Query("mode") String mode, @Query("sensor") boolean sensor, @Query("key") String key);
 
+    // google api
     @GET("api/distancematrix/json")
     Call<GoogleDistanceDurationResponse> getDistanceDuration(@Query("units") String units, @Query("origins") String origins, @Query("destinations") String destinations, @Query("mode") String mode, @Query("sensor") boolean sensor, @Query("key") String key);
 
-    @GET("v1/symptom/list")
+    @GET("/api/v1/symptom/list")
     Call<SymptomResponse> getSymptomList(@Query("page") int page, @Query("size") int size);
 
-    @DELETE("v1/users/logout")
+    @DELETE("/api/v1/users/logout")
     Call<EmptyResponse> logout();
 
-    @POST("v1/triage/endCall")
+    @POST("/api/v1/triage/endCall")
     Call<EmptyResponse> endCall(@Query("requestId") String requestId);
 
-    @POST("v1/users/submitUserFeedBack")
+    @POST("/api/v1/users/submitUserFeedBack")
     Call<EmptyResponse> submitUserFeedBack(@Body FeedbackRequest body);
 
-    @POST("v1/provider/submitProviderFeedBack")
+    @POST("/api/v1/provider/submitProviderFeedBack")
     Call<EmptyResponse> submitProviderFeedBack(@Body FeedbackRequest body);
 
-    @GET("v1/users/getUrgentCareList")
+    @GET("/api/v1/users/getUrgentCareList")
     Call<UrgentCaresResponse> getUrgentCareList(@Query("page") int page, @Query("size") int size, @Query("latitude") double latitude, @Query("longitude") double longitude);
 
     @GET("/api/v1/provider/getAedList")
     Call<UrgentCaresResponse> getAedList(@Query("page") int page, @Query("size") int size, @Query("latitude") double latitude, @Query("longitude") double longitude);
 
-    @GET("v1/users/getUserCompletedRequestList")
+    @GET("/api/v1/users/getUserCompletedRequestList")
     Call<EmsRequestResponse> getUserCompletedRequestList(@Query("page") int page, @Query("size") int size);
 
-    @GET("v1/provider/getProviderCompletedRequestList")
+    @GET("/api/v1/provider/getProviderCompletedRequestList")
     Call<EmsRequestResponse> getProviderCompletedRequestList(@Query("page") int page, @Query("size") int size);
 
-    @GET("v1/incidentReport/getIncidentReportDetail")
+    @GET("/api/v1/incidentReport/getIncidentReportDetail")
     Call<EmsRequestDetailsResponse> getIncidentReportDetail(@Query("requestId") String requestId);
 
-    @GET("v1/user/getDateDiff")
+    @GET("/api/v1/user/getDateDiff")
     Call<ServerTimeDiffResponse> getServerTimeDiff(@Query("date") long sysTime);
 
-    @POST("v1/provider/updateProfileDetail")
+    @POST("/api/v1/provider/updateProfileDetail")
     Call<EmptyResponse> updateProviderProfile(@Body User body);
 
-    @POST("v1/users/updateProfileDetail")
+    @POST("/api/v1/users/updateProfileDetail")
     Call<EmptyResponse> updateUserProfile(@Body User body);
 
-    @GET("v1/users/getProfileDetail")
+    @GET("/api/v1/users/getProfileDetail")
     Call<LoginResponse> getUserProfile();
 
-    @POST("v1/user/changePassword")
+    @POST("/api/v1/user/changePassword")
     Call<EmptyResponse> changePassword(@Body HashMap<String, Object> body);
 
-    @POST("v1/provider/updateProviderRegistration")
+    @POST("/api/v1/provider/updateProviderRegistration")
     Call<EmptyResponse> registerProvider(@Body ProviderRegistration registration);
 
-    @GET("v1/provider/getProfessionList")
+    @GET("/api/v1/provider/getProfessionList")
     Call<OccupationResponse> getOccupationList();
 
-    @GET("v1/provider/getCprTrainingInstituteList ")
+    @GET("/api/v1/provider/getCprTrainingInstituteList ")
     Call<CprTrainingInstitutesResponse> getCprInstituteList();
 }
